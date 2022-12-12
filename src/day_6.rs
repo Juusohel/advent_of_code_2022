f9f93635a47ea982486de512b43cc3f014375fd9
Juuso Helviö <juuso.helvio@ssh.com>
2022-12-12 16:08:50 +0200

    Day 6 - part 2

--- src/day_6.rs
+++ src/day_6.rs
@@ -14,27 +14,27 @@ pub fn processed_characters_count(filepath: &str) -> i32 {
         let line = line.unwrap();
         // just one line but eh
         let chars = line.chars();
-        let mut four: Vec<char> = Vec::new();
+        let mut checkstring: Vec<char> = Vec::new();
 
         for (i, char) in chars.enumerate() {
             // if less than 4, still in the beginning
-            if i < 3 {
-                four.push(char);
+            if i < 13 {
+                checkstring.push(char);
                 continue;
             }
-            four.push(char);
-            let unique = check_if_unique_four(&four);
+            checkstring.push(char);
+            let unique = check_if_unique_combo(&checkstring);
             if unique {
                 return i as i32 + 1;
             }
-            let _ = four.remove(0);
+            let _ = checkstring.remove(0);
         }
     }
 
     return 0;
 }
 
-fn check_if_unique_four(vec: &Vec<char>) -> bool {
+fn check_if_unique_combo(vec: &Vec<char>) -> bool {
     let mut map: HashSet<&char> = HashSet::new();
     for char in vec {
         if map.contains(&char) {
